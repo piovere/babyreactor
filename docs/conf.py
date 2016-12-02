f5af18183ed7389ed9c5119f59e9c87eca43e7c8
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2016-12-02 11:19:43 -0500

    docs now build for github-pages

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,8 @@ import babyreactor
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
