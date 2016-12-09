a4673021d1b4bd95bda817a092dbb9d50f823a6a
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2016-12-09 15:20:29 -0500

    cartesian matrix ready for testing

--- babyreactor/matrix.py
+++ babyreactor/matrix.py
@@ -5,11 +5,53 @@ from __future__ import division
 import numpy as np
 
 
-def cartesian():
+def cartesian(fuel, reflector, group):
     """
     Cartesian operator matrix
     """
-    pass
+    def matrix(material):
+        """
+        General cartesian material matrix
+        """
+        dx = material.delta()
+        diffusion = material.diffusion
+        removal = material.removal
+        sections = material.nodes - 1
+
+        diag_val = (removal + 2. * diffusion / dx ** 2)
+        offdiag_val = (- diffusion / dx ** 2)
+
+        diag = np.ones(sections) * diag_val
+        offdiag = np.ones(sections - 1) * offdiag_val
+
+        matr = np.diag(diag, 0) + np.diag(offdiag, -1) + np.diag(offdiag, 1)
+        matr[0, 0] = 0.5 * diag_val
+
+        return matr
+
+    # First make the fuel and reflector matrices
+    fuel_matrix = matrix(fuel)
+    r = reflector
+    reflector_matrix = matrix(reflector)
+
+    # Now join them
+    top_left = fuel_matrix
+    bottom_right = reflector_matrix
+    top_right = np.zeros((fuel.nodes, reflector.nodes - 2))
+    bottom_left = np.zeros_like(top_right).transpose()
+
+    top = np.concatenate((top_left, top_right), axis=1)
+    bottom = np.concatenate((bottom_left, bottom_right), axis=1)
+    everything = np.concatenate((top, bottom), axis=0)
+
+    everything[fuel.nodes - 1][fuel.nodes - 1] =\
+            -(1 + (r.diffusion * fuel.delta()) / (fuel.diffusion * r.delta()))
+    everything[fuel.nodes - 1][fuel.nodes - 2] = (r.diffusion * fuel.delta()) /\
+                                                 (fuel.diffusion * r.delta())
+    everything[fuel.nodes - 1][fuel.nodes] = (r.diffusion * fuel.delta()) /\
+                                             (fuel.diffusion * r.delta())
+
+    return everything
 
 def cylindrical():
     """
